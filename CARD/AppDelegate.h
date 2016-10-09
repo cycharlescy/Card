440fe5ceb3846098247bc206cdb92f6424ca3ec1
yaoxin <charlesyaoxin@163.com>
2016-10-09 11:34:44 +0800

    Initial Commit

---
+++ CARD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CARD
+//
+//  Created by yaoxin on 16/10/9.
+//  Copyright © 2016年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CARD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CARD
+//
+//  Created by yaoxin on 16/10/9.
+//  Copyright © 2016年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
